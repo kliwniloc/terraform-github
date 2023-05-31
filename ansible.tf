aa7737ec9c520a17df02f777231252f8a7dbb4b4
Colin Wilk <colin.wilk@tum.de>
2023-05-31 15:12:52 +0200

    Fix terraform formatting in ansible file
    Signed-off-by: Colin Wilk <colin.wilk@tum.de>

--- ansible.tf
+++ ansible.tf
@@ -1,12 +1,12 @@
 resource "github_repository" "ansible" {
   for_each = { for idx, repo in var.repos : idx => repo }
 
-  name        = "ansible-role-${each.value.name}"
-  description = each.value.description
-  topics      = each.value.topics
+  name         = "ansible-role-${each.value.name}"
+  description  = each.value.description
+  topics       = each.value.topics
   homepage_url = each.value.homepage_url
-  visibility  = each.value.visibility
-  archived    = each.value.archived
+  visibility   = each.value.visibility
+  archived     = each.value.archived
 
   has_discussions      = false
   has_issues           = true
@@ -25,33 +25,32 @@ resource "github_actions_secret" "example_secret" {
 
 variable "repos" {
   type = list(object({
-    name        = string
-    description = string
-    topics      = list(string)
+    name         = string
+    description  = string
+    topics       = list(string)
     homepage_url = string
-    visibility  = string
-    galaxy      = bool
-    archived    = bool
+    visibility   = string
+    galaxy       = bool
+    archived     = bool
   }))
   default = [
     {
-      name        = "prometheus-target"
-      description = "Ansible role for pushing targets to prometheus instance"
-      topics = ["ansible", "role", "galaxy", "prometheus", "monitoring",
-      "metrics", "prometheus-exporter", "node-exporter"]
+      name         = "prometheus-target"
+      description  = "Ansible role for pushing targets to prometheus instance"
+      topics       = ["ansible", "role", "galaxy", "prometheus", "monitoring", "metrics", "prometheus-exporter", "node-exporter"]
       homepage_url = "https://galaxy.ansible.com/kliwniloc/prometheus_target"
-      visibility = "public"
-      galaxy     = true
-      archived   = false
+      visibility   = "public"
+      galaxy       = true
+      archived     = false
     },
     {
-      name        = "borgbackup"
-      description = "Ansible role for deploying borgbackup on client and server"
-      topics      = ["ansible", "role", "galaxy", "backup", "borgbackup"]
+      name         = "borgbackup"
+      description  = "Ansible role for deploying borgbackup on client and server"
+      topics       = ["ansible", "role", "galaxy", "backup", "borgbackup"]
       homepage_url = "https://galaxy.ansible.com/kliwniloc/borgbackup"
-      visibility  = "private"
-      galaxy      = false
-      archived    = false
+      visibility   = "private"
+      galaxy       = false
+      archived     = false
     },
   ]
 }
