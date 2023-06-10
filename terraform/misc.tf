dedc7b30342442ddfb83f8e22e74986fa45ebcab
Colin Wilk <colin.wilk@tum.de>
2023-06-10 20:44:37 +0200

    Make terraform repository public
    Signed-off-by: Colin Wilk <colin.wilk@tum.de>

--- terraform/misc.tf
+++ terraform/misc.tf
@@ -3,11 +3,22 @@ resource "github_repository" "terraform-github" {
   description = "Terraform configuration managing my github repositories"
   topics      = ["terraform", "github"]
 
-  visibility = "private"
+  visibility = "public"
   archived   = false
 
   has_discussions = false
   has_projects    = false
   has_issues      = true
   homepage_url    = "https://github.com/kliwniloc"
+
+
+  vulnerability_alerts = true
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
 }
